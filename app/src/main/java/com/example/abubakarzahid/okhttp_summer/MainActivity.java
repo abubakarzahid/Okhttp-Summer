e6716d69ee52f43f5fedef4a4a1d0e7041713e51
abubakar zahid <abubakarzahid68@gmail.com>
2017-07-28 06:58:20 -0700

    okhttp getting request

--- app/src/main/java/com/example/abubakarzahid/okhttp_summer/MainActivity.java
+++ app/src/main/java/com/example/abubakarzahid/okhttp_summer/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.abubakarzahid.okhttp_summer;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,55 +15,43 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-    private  Button getbtn;
-    private TextView result;
-    private OkHttpClient client;
+
+
+    private static final String TAG = "MTAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        result = (TextView)findViewById(R.id.txtresponse);
-        getbtn = (Button)findViewById(R.id.btnrequest);
 
-        getbtn.setOnClickListener(new View.OnClickListener() {
+        Button btn = (Button)findViewById(R.id.btnrequest);
+
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getWebservice();
-            }
-        });
-        client = new OkHttpClient();
-    }
-    private void getWebservice(){
-        Request request = new Request.Builder()
-                .url("https://api.myjson.com/bins/10m5nz")
-                .build();
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                runOnUiThread(new Runnable() {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url("https://api.myjson.com/bins/1d3ei7")
+                        .build();
+
+                okHttpClient.newCall(request).enqueue(new Callback() {
+                   
+
                     @Override
-                    public void run() {
-                        result.setText("failure");
+                    public void onFailure(Call call, IOException e) {
+                        Log.d(TAG, "onFailure: ");
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+
+                        Log.d(TAG,"on Response :" + response.body().string());
                     }
                 });
             }
-
-            @Override
-            public void onResponse(Call call, final Response response)  {
-
-              runOnUiThread(new Runnable() {
-                  @Override
-                  public void run() {
-                      try {
-                          result.setText(response.body().string());
-                      } catch (IOException ioe){
-                          result.setText("Error getting messoage");
-                      }
-                  }
-              });
-            }
         });
+
+
 
 
     }
